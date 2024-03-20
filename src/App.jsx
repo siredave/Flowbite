5014c4fe2c9edc04c305a950770077896ee8eec4
siredave <siredave1@gmail.com>
2024-03-20 21:20:03 +0100

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
+import { Body } from './components/Body'
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar/>
+      <Footer/>
+      <Body/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+
+export const Navbar = () => {
+  return (
+    
+
+
+<button type="button" className="text-white bg-blue-700 ">Default eee</button>
+
+
+  )
+}
+
+
